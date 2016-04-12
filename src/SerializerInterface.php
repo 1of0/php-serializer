fe1821fb61f0b579f3842782d0fa54ed199fabfe
Bernardo <bernardo@thebigb.nl>
2016-04-12 10:36:43 +0200

    Extracted serialization methods from Serializer to interface

---
+++ src/SerializerInterface.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright (c) 2015 Bernardo van der Wal
+ * MIT License
+ *
+ * Refer to the LICENSE file for the full copyright notice.
+ */
+
+namespace OneOfZero\Json;
+
+interface SerializerInterface
+{
+	/**
+	 * @param mixed $data
+	 * @return string
+	 */
+	public function serialize($data);
+
+	/**
+	 * @param string $json
+	 * @param string|null $typeHint
+	 * @return mixed
+	 */
+	public function deserialize($json, $typeHint = null);
+}
