72390638228a4e02ac955c8864b77e17eeae5c6f
Bernardo <bernardo@thebigb.nl>
2016-04-19 14:34:10 +0200

    Implemented XML mapper (resolves #10)

---
+++ src/Mappers/XmlMapperFactory.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Copyright (c) 2016 Bernardo van der Wal
+ * MIT License
+ *
+ * Refer to the LICENSE file for the full copyright notice.
+ */
+
+namespace OneOfZero\Json\Mappers;
+
+use DOMAttr;
+use DOMDocument;
+use DOMElement;
+use DOMXPath;
+use Exception;
+use RuntimeException;
+
+class XmlMapperFactory extends ArrayMapperFactory
+{
+	/**
+	 * @param string $mappingFile
+	 */
+	public function __construct($mappingFile)
+	{
+		if (!file_exists($mappingFile))
+		{
+			throw new RuntimeException("File \"$mappingFile\" does not exist");
+		}
+
+		$document = new DOMDocument();
+		
+		if ($document->load($mappingFile) === false)
+		{
+			throw new RuntimeException("Failed parsing XML in \"$mappingFile\"");
+		}
+
+		try
+		{
+			$xpath = new DOMXPath($document);
+
+			/** @var DOMElement $alias */
+			foreach ($xpath->query('/mapping/use') as $alias)
+			{
+				$this->aliases[$alias->getAttribute('alias')] = $alias->getAttribute('fqn');
+			}
+
+			/** @var DOMElement $class */
+			foreach ($xpath->query('/mapping/class') as $class)
+			{
+				$className = $class->getAttribute('name');
+
+				/** @var DOMAttr $attribute */
+				foreach ($class->attributes as $attribute)
+				{
+					$this->mapping[$className][$attribute->name] = $attribute->value;
+				}
+
+				/** @var DOMElement $converters */
+				foreach ($xpath->query('converters[1]', $class) as $converters)
+				{
+					$this->mapping[$className]['converters']['serializer'] = $converters->getAttribute('serializer');
+					$this->mapping[$className]['converters']['deserializer'] = $converters->getAttribute(
+						'deserializer'
+					);
+				}
+
+				/** @var DOMElement $property */
+				foreach ($xpath->query('properties/*', $class) as $property)
+				{
+					/** @var DOMAttr $attribute */
+					foreach ($property->attributes as $attribute)
+					{
+						$this->mapping[$className]['properties'][$property->tagName][$attribute->name] = $attribute->value;
+					}
+
+					/** @var DOMElement $converters */
+					foreach ($xpath->query('converters[1]', $property) as $converters)
+					{
+						$this->mapping[$className]['properties'][$property->tagName]['converters']['serializer'] = $converters->getAttribute(
+							'serializer'
+						);
+						$this->mapping[$className]['properties'][$property->tagName]['converters']['deserializer'] = $converters->getAttribute(
+							'deserializer'
+						);
+					}
+				}
+
+				/** @var DOMElement $method */
+				foreach ($xpath->query('methods/*', $class) as $method)
+				{
+					/** @var DOMAttr $attribute */
+					foreach ($method->attributes as $attribute)
+					{
+						$this->mapping[$className]['methods'][$method->tagName][$attribute->name] = $attribute->value;
+					}
+				}
+			}
+		}
+		catch (Exception $e)
+		{
+			throw new RuntimeException("Failed parsing XML in \"$mappingFile\"");
+		}
+	}
+}
---
+++ test/XmlMapperTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright (c) 2016 Bernardo van der Wal
+ * MIT License
+ *
+ * Refer to the LICENSE file for the full copyright notice.
+ */
+
+namespace OneOfZero\Json\Test;
+
+use OneOfZero\Json\Mappers\MapperPipeline;
+use OneOfZero\Json\Mappers\ReflectionMapperFactory;
+use OneOfZero\Json\Mappers\XmlMapperFactory;
+use RuntimeException;
+
+class XmlMapperTest extends AbstractMapperTest
+{
+	const XML_MAPPING_FILE = __DIR__ . '/Assets/mapping.xml';
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function getPipeline()
+	{
+		return (new MapperPipeline)
+			->addFactory(new XmlMapperFactory(self::XML_MAPPING_FILE))
+			->addFactory(new ReflectionMapperFactory())
+			->build($this->defaultConfiguration)
+		;
+	}
+
+	public function testInvalidMapperFile()
+	{
+		$this->setExpectedException(RuntimeException::class);
+		new XmlMapperFactory('non-existing.xml');
+	}
+}
