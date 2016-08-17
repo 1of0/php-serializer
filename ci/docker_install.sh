ad47e5aa4bf9f2ea9c30894f6d70cbb5adc41c99
Bernardo <bernardo@thebigb.nl>
2016-08-17 21:58:15 +0200

    Minor fixes to container provisioning

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git -yqq
+apt-get install git unzip -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
@@ -15,3 +15,5 @@ chmod +x /usr/local/bin/phpunit
 
 curl --location --output /usr/local/bin/composer https://getcomposer.org/composer.phar
 chmod +x /usr/local/bin/composer
+
+echo "date.timezone = UTC" >> /usr/local/etc/php/conf.d/test.ini
