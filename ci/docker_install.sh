167ab2d095ba07698732ef95c602670a0dd616ed
Bernardo <bernardo@thebigb.nl>
2016-08-17 22:38:15 +0200

    Trying Alpine images for quicker builds

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# We need to install dependencies only for Docker
 [[ ! -e /.dockerenv ]] && [[ ! -e /.dockerinit ]] && exit 0
 
 set -xe
 
-# Install git (the php image doesn't have it) which is required by composer
-apt-get update -yqq
-apt-get install git unzip -yqq
+apk add --no-cache git unzip curl
 
-# Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
